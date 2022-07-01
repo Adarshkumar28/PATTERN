7b37e5914ff3244d22781b8e12b4d68f4ecb6146
Adarshkumar28 <106953411+Adarshkumar28@users.noreply.github.com>
2022-07-01 13:09:58 +0530

    Add files via upload

---
+++ Twelve.java
@@ -0,0 +1,20 @@
+class Twelve
+{
+public static void main(String[] args)
+{
+   int i,j;
+  
+  
+       for(i=1; i<=5; i++)
+       {
+            int k=i;
+            for(j=1; j<=i; j++)
+            {
+            System.out.print(k+" ");
+            k=k+5;
+            }
+       System.out.println();
+       }
+
+ }
+}
